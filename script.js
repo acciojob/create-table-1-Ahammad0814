282b650720af867772a88b437d82ca1a5b9cc0bb
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2025-02-18 19:08:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function insert_Row() {
 	//Write your code here
 	const newRow = `<tr><td>New cell1</td> 
-		<td>New cell2</td></tr>`
+		<td>New cell2</td></tr>`;
 
 	const oldRows = document.getElementById("sampleTable").innerHTML;
 
