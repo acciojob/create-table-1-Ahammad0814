7c56732f25bff737f487ee79add9ac13cd778083
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2025-02-18 19:06:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+	//Write your code here
+	const newRow = `<tr><td>New cell1</td> 
+		<td>New cell2</td></tr>`
+
+	const oldRows = document.getElementById("sampleTable").innerHTML;
+
+	document.getElementById("sampleTable").innerHTML = newRow + oldRows;
 }
